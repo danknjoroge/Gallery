788711758587da757660c07c9e3784ab20c1ebd2
danknjoroge <dank8505@gmail.com>
2022-03-23 21:13:58 +0300

    added image model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,9 @@
+from pickle import FALSE
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length=55)
+    description = models.TextField()
+    image = models.ImageField(upload_to = 'gallery/', null=FALSE, blank=FALSE)
+
